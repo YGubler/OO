e5ef8038f02115d262f9838e4886d17c5bda2d51
Yanick Gubler <ygubler@gmail.com>
2014-10-21 17:16:12 +0200

    vor Mikes Hilfe

--- Uebung5/src/RecursiveBracketChecker.java
+++ Uebung5/src/RecursiveBracketChecker.java
@@ -5,13 +5,72 @@ public class RecursiveBracketChecker {
 	}
 	
 	private boolean validate(StringReader reader) {
-		if(!reader.hasCurrent()){
+		char current = '0';
+		//hat es ein Zeichen?
+		if (reader.hasCurrent()) {
+			current = reader.current();
+			//ist da aktuelle Zeichen ein Öffnungsklammer, wenn ja hole das nächste Zeichen und rufe validate wieder auf.?
+			if (isOpeningBracket(current)){
+				reader.next();			
+				return validate(reader);
+			}
+			else if (!isOpeningBracket(current)){
+				return false;
+			}
+			else
+				reader.next();
+				char next = reader.current();
+				if (matches(current, next)){
+					return validate(reader);
+				}
+				else return false;
+		}
+		else if (!reader.hasCurrent() && current == 0){
 			return true;
 		}
-//		else if ()
-		
-		return false;
-	}
+		else return false;
+		}
+
+//		//Ist das aktuelle Zeichen keine offene Klammer, dann stimmen die Klammern
+//		else if (!isOpeningBracket(reader.current())){
+//			return true;
+//		}
+//		//nächstes Zeichen einlesen
+//		reader.next();
+//		
+//		//
+//		else if(){
+//			return 
+//			
+//		}
+//		else{
+//			reader.next();
+//			return validate(reader);
+//		}
+//			
+//			
+//			
+//		if (!reader.hasCurrent()) {
+//			return true;
+//		}
+//		char opening = reader.current();
+//		if (!isOpeningBracket(opening)) {
+//			return true;
+//		}
+//		reader.next();
+//		if (!validate(reader)) {
+//			return false;
+//		}
+//		if (!reader.hasCurrent()) {
+//			return false;
+//		}
+//		char closing = reader.current();
+//		if (!matches(opening, closing)) {
+//			return false;
+//		}
+//		reader.next();
+//		return validate(reader);
+	
 
 	private boolean isOpeningBracket(char character) {
 		return  character == '(' || 
