a495e04495593e7db56086ef572e5e5f5415bc7d
Yanick Gubler <ygubler@gmail.com>
2014-11-13 13:24:40 +0100

    Uebung9 aufgabe4 anstaht funktioniert auch aber anders aber noch mit arraylist

--- Uebung9/src/aufgabe4_PrimzahlenStrahl/Main.java
+++ Uebung9/src/aufgabe4_PrimzahlenStrahl/Main.java
@@ -4,32 +4,49 @@ import java.awt.List;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.function.IntConsumer;
 import java.util.stream.IntStream;
 import java.util.stream.Collectors;
 
 class Main {
+	
+	static ArrayList<Integer> primeList = new ArrayList<Integer>();
 
 	public static void main(String[] args) {	
+		System.out.println("Wie viele Primzahlen sollen berechnet werden?: ");
+		final int anzPrime = numFromConsole();
 		
-		ArrayList<Integer> primeList = 
-		(ArrayList<Integer>) IntStream
+		System.out.println("Die wievielte Primzahl möchten Sie wissen?: ");
+		
+		
+	
+		
+//		ArrayList<Integer> primeList = 
+//		(ArrayList<Integer>)
+		IntStream
 		.iterate(2, i -> i+1)
-		.limit(100000)
+//		.limit(100000)
 		.filter(Main::ifPrime)
-		.boxed()
-		.collect(Collectors.toList());
+		.limit(anzPrime)
+//		.skip(anzPrime-1)
+//		.findFirst()
+//		.boxed()
+//		.collect(Collectors.toList());
+		.forEach(Main::addArrayList);
 		
+		
+		System.out.println(getPrimeAt(primeList, numFromConsole()));
 				
-		int count = 0, maxTries = 3;
-		while(true){
-			try{
-				System.out.println("Die wievielte Primzahl möchten Sie wissen?: ");
-				System.out.println(getPrimeAt(primeList, numFromConsole()));
-			}catch (Exception e) {
-				System.err.println("aktuelle ArrayListlänge zu kurz: " + primeList.size());
-				if (++count == maxTries) System.out.println("Anzahl Versuche erreicht");;
-			}
-		}
+//		int count = 0, maxTries = 3;
+//		while(true){
+//			try{
+//				System.out.println("Die wievielte Primzahl möchten Sie wissen?: ");
+//				System.out.println(getPrimeAt(primeList, numFromConsole()));
+//			}catch (Exception e) {
+//				System.err.println("aktuelle ArrayListlänge zu kurz: " + primeList.size());
+//				if (++count == maxTries) System.out.println("Anzahl Versuche erreicht");;
+//			}
+//		}
 	
 		
 //		IntStream
@@ -39,6 +56,11 @@ class Main {
 //		.forEach(System.out::println);
 		
 
+	}
+	
+	public static void addArrayList(int prime){
+		primeList.add(prime);
+//		System.out.println("huhu");
 	}
 
 	public static boolean ifPrime(int n) {
@@ -53,9 +75,10 @@ class Main {
 	}
 	
 	public static int numFromConsole(){
+		System.out.println("test num");
 		Scanner numFromConsole = new Scanner(System.in);
 		int num = numFromConsole.nextInt();
-		numFromConsole.close();
+//		numFromConsole.close();
 		return num;
 	}
 	
