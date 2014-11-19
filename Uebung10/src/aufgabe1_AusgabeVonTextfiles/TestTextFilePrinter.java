4ab677c9ed2ac9d2f6bddb224dbcb2a3cc147d85
Yanick Gubler <ygubler@gmail.com>
2014-11-19 09:57:31 +0100

    Uebung 10 Aufg1 vor 2. teil

--- Uebung10/src/aufgabe1_AusgabeVonTextfiles/TestTextFilePrinter.java
+++ Uebung10/src/aufgabe1_AusgabeVonTextfiles/TestTextFilePrinter.java
@@ -6,11 +6,11 @@ import java.io.IOException;
 class TestTextFilePrinter {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		String path = "src/aufgabe1_AusgabeVonTextfiles/Text.txt";
-		TextFilePrinter.fileReadAndPrint(path);
+//		String path = "src/aufgabe1_AusgabeVonTextfiles/Text.txt";
+//		TextFilePrinter.fileReadAndPrint(path);
 		
-		String path = "src/aufgabe1_AusgabeVonTextfiles/Text.txt";
-		TextFilePrinter.fileReadAndPrint(path);
+		String path2 = "src/aufgabe1_AusgabeVonTextfiles/TextUtf8.txt";
+		TextFilePrinter.fileReadAndPrint(path2);
 
 
 	}
