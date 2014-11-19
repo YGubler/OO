bcc36f358d85dfeefb2d1de642dfb9a54f0e88e5
Yanick Gubler <ygubler@gmail.com>
2014-11-19 10:04:00 +0100

    Uebung 10 Aufgabe 1 fertig

--- Uebung10/src/aufgabe1_AusgabeVonTextfiles/TestTextFilePrinter.java
+++ Uebung10/src/aufgabe1_AusgabeVonTextfiles/TestTextFilePrinter.java
@@ -6,9 +6,10 @@ import java.io.IOException;
 class TestTextFilePrinter {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-//		String path = "src/aufgabe1_AusgabeVonTextfiles/Text.txt";
-//		TextFilePrinter.fileReadAndPrint(path);
+		String path = "src/aufgabe1_AusgabeVonTextfiles/Text.txt";
+		TextFilePrinter.fileReadAndPrint(path);
 		
+		System.out.println("\n\n");
 		String path2 = "src/aufgabe1_AusgabeVonTextfiles/TextUtf8.txt";
 		TextFilePrinter.fileReadAndPrint(path2);
 
--- Uebung10/src/aufgabe1_AusgabeVonTextfiles/TextFilePrinter.java
+++ Uebung10/src/aufgabe1_AusgabeVonTextfiles/TextFilePrinter.java
@@ -1,30 +1,35 @@
 package aufgabe1_AusgabeVonTextfiles;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 
-class TextFilePrinter {	
+class TextFilePrinter {
 
-	public static void fileReadAndPrint(String path) throws FileNotFoundException, IOException{
-		try (FileReader reader = new FileReader(path)) {
-			int textValue = reader.read();
-			int i = 1;
-			System.out.print(i++ + "\t");
-			while(textValue >= 0){
-				char c = (char) textValue;
-				if (c == '\n'){
-					System.out.print(c);
-					textValue = reader.read();
-					System.out.print(i++ + "\t");
-					
-				}else {
-					System.out.print(c);
-					textValue = reader.read();
+	public static void fileReadAndPrint(String path) throws FileNotFoundException, IOException {
+		try (InputStream byteStream = new FileInputStream(path)) {
+			try (Reader reader = new InputStreamReader(byteStream, "UTF-8")) {
+
+				int textValue = reader.read();
+				int i = 1;
+				System.out.print(i++ + "\t");
+				while (textValue >= 0) {
+					char c = (char) textValue;
+					if (c == '\n') {
+						System.out.print(c);
+						textValue = reader.read();
+						System.out.print(i++ + "\t");
+
+					} else {
+						System.out.print(c);
+						textValue = reader.read();
+					}
 				}
 			}
 		}
-	
+
 	}
 }
-
