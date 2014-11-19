c19a5be1bf7fb151ad856cb923fb0d980c1c1899
Yanick Gubler <ygubler@gmail.com>
2014-11-19 11:53:26 +0100

    Uebung 10 Aufgabe 3

---
+++ Uebung10/src/aufgabe3_StoryRekonstruktion/TestStoryRestore.java
@@ -0,0 +1,13 @@
+package aufgabe3_StoryRekonstruktion;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+class TestStoryRestore {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		storyRestore.restoreStory();
+
+	}
+
+}
---
+++ Uebung10/src/aufgabe3_StoryRekonstruktion/storyRestore.java
@@ -0,0 +1,32 @@
+package aufgabe3_StoryRekonstruktion;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+class storyRestore {
+	
+	public static void restoreStory() throws FileNotFoundException, IOException{
+		Map<Integer, String> storyMap = new TreeMap<>();
+		String ausgabe = "";
+		try (FileReader file = new FileReader("/Users/Yanick/Dropbox/Schule/HSR/Module/OO/00-Eigene/Workspace/Uebung10/src/aufgabe3_StoryRekonstruktion/story-input.txt")){
+			try (BufferedReader buffreader = new BufferedReader(file)){
+				String zeile = buffreader.readLine();
+				while (zeile != null && zeile.contains("=")){
+					String[] zeilenParts = zeile.split("=");
+					int key = Integer.parseInt(zeilenParts[0]);
+					String wort = zeilenParts[1];
+					storyMap.put(key, wort);
+					zeile = buffreader.readLine();
+				}
+			}		
+		}
+		
+		for(String story : storyMap.values()){
+			System.out.print(story + " ");
+		}
+	}
+}
