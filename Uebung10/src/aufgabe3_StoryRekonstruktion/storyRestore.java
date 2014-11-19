ab3795169d98133098e033b383c4708bb78c1c4d
Yanick Gubler <ygubler@gmail.com>
2014-11-19 11:58:40 +0100

    Uebung 10 Aufgabe 3 mit UTF-8

--- Uebung10/src/aufgabe3_StoryRekonstruktion/storyRestore.java
+++ Uebung10/src/aufgabe3_StoryRekonstruktion/storyRestore.java
@@ -1,9 +1,11 @@
 package aufgabe3_StoryRekonstruktion;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -12,7 +14,7 @@ class storyRestore {
 	public static void restoreStory() throws FileNotFoundException, IOException{
 		Map<Integer, String> storyMap = new TreeMap<>();
 		String ausgabe = "";
-		try (FileReader file = new FileReader("/Users/Yanick/Dropbox/Schule/HSR/Module/OO/00-Eigene/Workspace/Uebung10/src/aufgabe3_StoryRekonstruktion/story-input.txt")){
+		try (InputStreamReader file = new InputStreamReader(new FileInputStream("/Users/Yanick/Dropbox/Schule/HSR/Module/OO/00-Eigene/Workspace/Uebung10/src/aufgabe3_StoryRekonstruktion/story-input.txt"), "UTF-8")){
 			try (BufferedReader buffreader = new BufferedReader(file)){
 				String zeile = buffreader.readLine();
 				while (zeile != null && zeile.contains("=")){
